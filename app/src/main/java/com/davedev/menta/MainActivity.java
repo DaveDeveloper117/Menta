82092e574984efdba3363a30029dd3deca0121b0
DaveDeveloper117 <dabicho803@gmail.com>
2022-10-02 16:24:30 -0500

    init-commit

--- app/src/main/java/com/davedev/menta/MainActivity.java
+++ app/src/main/java/com/davedev/menta/MainActivity.java
@@ -2,13 +2,44 @@ package com.davedev.menta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.davedev.menta.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView logoImageView;
+    TextView titleTextView;
+    Animation fadeIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Logo Image
+        logoImageView = findViewById(R.id.logoImageView);
+        //Text Logo
+        titleTextView = findViewById(R.id.titleTextView);
+        //Animation
+        fadeIn = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+
+        //Set Animations
+        logoImageView.setAnimation(fadeIn);
+        titleTextView.setAnimation(fadeIn);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
---
+++ app/src/main/java/com/davedev/menta/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.davedev.menta.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.davedev.menta.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
