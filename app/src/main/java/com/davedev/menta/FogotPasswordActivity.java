81d5871686775a2c961a1dff12413778aad7f189
David-Laptop <dabicho803@gmail.com>
2022-10-03 11:47:55 -0500

    init-commit

--- app/src/main/java/com/davedev/menta/FogotPasswordActivity.java
+++ app/src/main/java/com/davedev/menta/FogotPasswordActivity.java
@@ -2,13 +2,28 @@ package com.davedev.menta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FogotPasswordActivity extends AppCompatActivity {
+    TextView registerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fogot_password);
+
+        registerTextView = findViewById(R.id.registerTextView);
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FogotPasswordActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/davedev/menta/LoginActivity.java
+++ app/src/main/java/com/davedev/menta/LoginActivity.java
@@ -8,15 +8,24 @@ import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView registerTextView;
+    TextView registerTextView, forgotPassTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        forgotPassTextView = findViewById(R.id.forgotPassTextView);
         registerTextView = findViewById(R.id.registerTextView);
 
+        forgotPassTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, FogotPasswordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
