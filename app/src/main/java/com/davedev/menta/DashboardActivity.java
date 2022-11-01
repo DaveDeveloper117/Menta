20b3bab86ec4ec8afb34a7cb636b1d5dd2736be4
DaveDeveloper117 <dabicho803@gmail.com>
2022-10-31 18:21:16 -0600

    init-commit

--- app/src/main/java/com/davedev/menta/DashboardActivity.java
+++ app/src/main/java/com/davedev/menta/DashboardActivity.java
@@ -1,8 +1,10 @@
 package com.davedev.menta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -39,6 +41,60 @@ public class DashboardActivity extends AppCompatActivity {
         fabLogo.setAnimation(downMove);
         titleTextView.setAnimation(upMove);
         scrollCards.setAnimation(fadeIn);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        if (isDarkModeOn) {
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_YES);
+
+        }
+        else {
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_NO);
+
+        }
+
+        fabLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isDarkModeOn) {
+
+                    // if dark mode is on it
+                    // will turn it off
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                    // it will set isDarkModeOn
+                    // boolean to false
+                    editor.putBoolean(
+                            "isDarkModeOn", false);
+                    editor.apply();
+                }
+                else {
+
+                    // if dark mode is off
+                    // it will turn it on
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+
+                    // it will set isDarkModeOn
+                    // boolean to true
+                    editor.putBoolean(
+                            "isDarkModeOn", true);
+                    editor.apply();
+                }
+            }
+        });
 
         interiorPlants.setOnClickListener(new View.OnClickListener() {
             @Override
